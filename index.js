6921a09fc65b2c9f2d5ef07f9114d2e82cb1f5ad
Ronnie <agustine103@gmail.com>
2014-08-21 17:24:46 +0800

    -

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
  * Created by yexiaoyi on 14-8-20.
  */
 'use strict';
+var fs = require('fs');
+var path = require('path');
 var _ = require('lodash');
 var chalk = require('chalk');
 
@@ -11,35 +13,109 @@ exports.usage = '<src> [options]';
 
 exports.options = {
     'src': {
-        alias : 's',
-        default: 'template/'
+        alias: 's',
+        default: 'template/*.html',
+        describe: 'template folder path(模版文件所在目录)'
     },
-    'dest' : {
-        alias : 'd'
-        ,describe : 'target pack file path'
+    templateName: {
+        alias: 't',
+        default: function (fileName) {
+            if (!fileName) {
+                return '';
+            }
+            return fileName[0] === '_' ? '' : fileName;
+        },
+        describe: 'function for parse the file name to template function name(把文件名转换成调用模版的方法名)'
     },
-    'action': {
-        alias : 'a'
-        ,default: 'create'
-        ,describe : 'create or extract'
+    'dest': {
+        alias: 'd',
+        default: 'js/template.js',
+        describe: 'target pack file path(生成jst文件路径)'
+    },
+    charset: {
+        alias: 'c',
+        default: 'UTF-8',
+        describe: 'charset of template'
+    },
+    templateSettings: {
+        alias: 'c',
+        default: {
+            evaluate: /<%([\s\S]+?)%>/g,
+            interpolate: /<%=([\s\S]+?)%>/g,
+            escape: /<%-([\s\S]+?)%>/g,
+            partial: /<%@([\s\S]+?)%>/g
+        },
+        describe: 'charset of template'
     }
 };
 
-exports.run = function (options, done) {
-    var source = options.src;
-    var target = options.dest;
-    var action = options.action;
-    exports[action](source, target, done);
+exports.run = function (options) {
+    var file = exports.file;
+    var charset = options.charset;
+    var outputFile = options.dest;
+    var outputSource = '';
+    exports.files.forEach(function (inputFile) {
+        var extName = path.extname(inputFile);
+        var fileName = path.basename(inputFile, extName);
+        var templateName = options.templateName(fileName);
+        var fileContent;
+
+        if (templateName) {
+            fileContent = file.read(inputFile, charset);
+
+            fileContent = exports.partialTemplate(inputFile, fileContent, charset, options);
+
+            fileContent = exports.generate(templateName, fileContent);
+
+            outputSource = outputSource ? (outputSource + ',') : '';
+            outputSource += '\n' + fileContent;
+
+            exports.log('template ' + chalk.cyan(inputFile) + ' is compiled.');
+        }
+    });
+    outputSource = 'var JST = {\n' + outputSource + '\n}';
+    file.write(outputFile, outputSource, charset);
 };
 
-exports.create = function(source, target, done){
-    if(!target && exports.file.findPackageJSON(source)){
-        var cfg = exports.file.readPackageJSON(source);
-        target = cfg.name + '-' + cfg.version + '.tar.gz';
+exports.partialTemplate = function (fileName, fileContent, charset, options) {
+    var file = exports.file;
+    var refered = [fileName];
+    var referedTemp = [];
+    try {
+        while (options.templateSettings.partial.test(fileContent)) {
+            fileContent = fileContent.replace(options.templateSettings.partial, function (replaceStatement, name) {
+                var partialName = _.find(exports.files, function (file) {
+                    return path.basename(file).toLowerCase() === name.toLowerCase();
+                });
+                if (!partialName) {
+                    exports.warn('partial template ' + chalk.cyan(name) + ' is not exists.');
+                }
+                if (_.contains(refered, partialName)) {
+                    throw 'An infinite loop may occur in importing partial template';
+                }
+                referedTemp.push(partialName);
+                return partialName ? file.read(partialName, charset) : '';
+            });
+            Array.prototype.push.apply(refered, referedTemp);
+            referedTemp = [];
+        }
+        return fileContent;
+    } catch (e) {
+        exports.error(e);
+        exports.warn('JST ' + chalk.cyan(templateName) + ' failed to compile.');
+        throw e;
     }
-    tar.create(source, target, cfg, exports, done);
 };
 
-exports.extract = function(source, target, done) {
-    tar.extract(source, target, exports, done);
+exports.generate = function (templateName, fileContent) {
+    var compiled;
+    try {
+        compiled = _.template(fileContent, false).source;
+    } catch (e) {
+        exports.error(e);
+        exports.warn('JST ' + chalk.cyan(templateName) + ' failed to compile.');
+        throw e;
+    }
+    return templateName + ':' + compiled;
 };
+
